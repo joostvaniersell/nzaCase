60afc6b7e27709b289f8ba02fa63674460f2e810
joostvaniersell <74365305+joostvaniersell@users.noreply.github.com>
2023-03-06 16:54:50 +0100

    Added: -tab and ui for nyc airport analysis -first reactive graph for amount of visitors

--- server.R
+++ server.R
@@ -4,6 +4,8 @@ library(sf)
 library(leaflet)
 library(nycflights13)
 library(DT)
+library(plotly)
+library(lubridate)
 
 #load in datasets for raw datatab
 flights_df <- nycflights13::flights
@@ -13,11 +15,31 @@ planes_df <- nycflights13::planes
 weather_df <-  nycflights13::weather
 
 
+flights_info <- flights_df %>% full_join(planes, "tailnum")  #Joint de plane info dataset met de flights info dataset
+
+flights_info <- 
+  flights_info %>% 
+  mutate(seats = replace_na(seats,mean(seats, na.rm = TRUE))) #replace missings
+
+
+
+flights <-
+  flights_info %>%
+  group_by(month, origin, day)%>%
+  summarise(seats= sum(seats)) %>%
+  ungroup
+
+flights <- flights %>% mutate(flights,date=as.Date(paste('2013',month,day,sep="/")))
+
+flights$date <-  as.Date(parse_date_time(flights$date, "%y/%m/%d"))
+
+
 
 #start up render function
 
 function(input, output) {
   
+  ###############################################
   #add a leaflet map for output
   output$map <- renderLeaflet({
     leaflet() %>%
@@ -28,27 +50,52 @@ function(input, output) {
   
 
   
+  ###############################################
+ 
+  
+  #generate user-inputed graphs for analzying
   
   
+  flight_reactive <- reactive({
+    filter(flights, date >= input$daterange[1] & date <= input$daterange[2], origin %in% input$airportChoice)
+  })
+  
+  output$passenger_numbers <- renderPlotly({
+    
+    
+    p <- ggplot(data = flight_reactive(), aes(fill= origin, x= date, y= seats)) +
+      geom_bar(position = 'stack', stat="identity", na.rm = TRUE)
+
+    p <- ggplotly(p)
+    p
+  })
+  
+  
+  
+  
+  
+  
+  
+  ###########################################
   
   #generate datatables for raw data output
   output$flights = DT::renderDataTable({
-    datatable(flights_df, filter = 'top')
+    DT::datatable(flights_df, filter = 'top',  options = list(orderClasses = TRUE))
   })
   
   output$airlines = DT::renderDataTable({
-    datatable(airlines_df, filter = 'top')
+    DT::datatable(airlines_df, filter = 'top',  options = list(orderClasses = TRUE))
   })
   
   output$airports = DT::renderDataTable({
-    datatable(airports_df, filter = 'top')
+    DT::datatable(airports_df, filter = 'top',  options = list(orderClasses = TRUE))
   })
   
   output$planes = DT::renderDataTable({
-    datatable(planes_df, filter = 'top')
+    DT::datatable(planes_df, filter = 'top',  options = list(orderClasses = TRUE))
   })
   
   output$weather = DT::renderDataTable({
-    datatable(weather_df, filter = 'top')
+    DT::datatable(weather_df, filter = 'top',  options = list(orderClasses = TRUE))
   })
 }
--- ui.R
+++ ui.R
@@ -3,7 +3,9 @@ library(shiny)
 library(sf)
 library(leaflet)
 library(nycflights13)
-
+library(DT)
+library(plotly)
+library(lubridate)
 #create a navigation bar with seperate tabs
 navbarPage("NYC Flights", id="nav", 
            
@@ -15,7 +17,32 @@ navbarPage("NYC Flights", id="nav",
           ),
           
           #create second tab: data explorer
-          tabPanel("Data explorer"),
+          tabPanel("NY Airports explorer",
+            
+
+            sidebarLayout(
+              #create sidebar for inputs
+              sidebarPanel(
+                checkboxGroupInput(inputId = "airportChoice", label = h3("Which airport(s) would you like to analyze"), 
+                                   choices = c("JFK", "EWR", "LGA"),
+                                   selected = c("JFK", "EWR" , "LGA")
+                                   ),
+
+                dateRangeInput(inputId= "daterange", "During which time perio:",
+                               start = "2013-01-01",
+                               end   = "2013-12-31")
+                ),
+              
+              #create mainpanel for outputs, e.g. graphs
+              mainPanel(tabsetPanel(
+                tabPanel('Number of passengers', plotlyOutput('passenger_numbers')),
+                tabPanel('Destinations'),
+                
+              )
+              )
+          )
+          ),
+          
           
           #create third tab: raw data explorer
           tabPanel("Raw Data explorer",
